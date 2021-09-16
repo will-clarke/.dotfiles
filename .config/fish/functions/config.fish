2c44441833bfab27dc6b5251dbc877b9bf7838d7
William Clarke <wmmclarke@gmail.com>
2021-09-16 12:47:59 +0100

    more fish

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,3 @@
-function config
-         git --git-dir=$HOME/.cfg/ --work-tree=$HOME
+function config --wraps='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' --description 'alias config=/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+  /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv; 
 end
