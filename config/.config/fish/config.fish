cdad66ae9727747cb9beae565462f5ce1f4bbf5a
William Clarke <wmmclarke@gmail.com>
2021-11-27 14:30:07 +0000

    remove variables shortcut in fish (\cv)

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -4,4 +4,5 @@ if status is-interactive
     abbr --add --global gd git diff
     abbr --add --global gs git status
     abbr --add --global gdc git diff --cached
+    fzf_configure_bindings --variables # C-V is an annoying default shortcut to use so we should disable it
 end
