94337a2b34f1b4033b1686aeea18d828324ff30c
William Clarke <wmmclarke@gmail.com>
2022-03-30 11:15:20 +0100

    Commit the shiftit hammerspoon lua script

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,20 @@
+hs.loadSpoon("ShiftIt")
+
+spoon.ShiftIt:bindHotkeys({
+ rleft = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'h' },
+  right = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'l' },
+  up = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'k' },
+  down = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'j' },
+  upleft = {{ 'ctrl', 'alt', 'cmd', 'shift' }, '1' },
+  upright = {{ 'ctrl', 'alt', 'cmd', 'shift' }, '2' },
+  botleft = {{ 'ctrl', 'alt', 'cmd', 'shift' }, '3' },
+  botright = {{ 'ctrl', 'alt', 'cmd', 'shift' }, '4' },
+  maximum = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'm' },
+  toggleFullScreen = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'f' },
+  toggleZoom = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'z' },
+  center = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'c' },
+  nextScreen = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'n' },
+  previousScreen = {{ 'ctrl', 'alt', 'cmd', 'shift' }, 'p' },
+  resizeOut = {{ 'ctrl', 'alt', 'cmd', 'shift' }, '=' },
+  resizeIn = {{ 'ctrl', 'alt', 'cmd', 'shift' }, '-' }
+});
