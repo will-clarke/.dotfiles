9af3b6c27df292a98c6a67611fa84e98dde341e2
Will Clarke <will.clarke@justeattakeaway.com>
2022-12-13 08:47:49 +0000

    add starship.toml

--- docs/setup_new_mac.sh
+++ docs/setup_new_mac.sh
@@ -2,34 +2,60 @@
 
 mkdir -p ~/.config/{karabiner,helix,lf}
 
-curl https://git.sr.ht/~will-clarke/.dotfiles/blob/master/config/.config/karabiner/karabiner.json -o ~/.config/karabiner/karabiner.json
-curl https://git.sr.ht/~will-clarke/.dotfiles/blob/master/config/.config/helix/config.toml -o ~/.config/helix/config.toml
+cd && git clone https://git.sr.ht/~will-clarke/.dotfiles
 
-for f in lf_kitty_clean lf_kitty_preview lfrc
-do
-curl https://git.sr.ht/~will-clarke/.dotfiles/blob/master/config/.config/lf/$f -o ~/.config/lf/$f
-done
+# curl https://git.sr.ht/~will-clarke/.dotfiles/blob/master/config/.config/karabiner/karabiner.json -o ~/.config/karabiner/karabiner.json
+# curl https://git.sr.ht/~will-clarke/.dotfiles/blob/master/config/.config/helix/config.toml -o ~/.config/helix/config.toml
+
+# for f in lf_kitty_clean lf_kitty_preview lfrc
+# do
+# curl https://git.sr.ht/~will-clarke/.dotfiles/blob/master/config/.config/lf/$f -o ~/.config/lf/$f
+# done
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install shiftit --cask
-brew install karabiner-elements --cask
-brew install fish
-brew install gpg
-brew install pass
-brew install pass-otp
-brew install bat
-brew install lf
-brew install highlight
-brew install coreutils
-brew install zoxide
-brew install fzf
-brew install insomnia --cask
-brew install google-chrome --cask
-brew install --cask slack
-brew install gnu-sed
-brew install fd
+brew tap homebrew/cask-fonts
+
+brew install fish \
+  gpg \
+  pass \
+  pass-otp \
+  bat \
+  lf \
+  highlight \
+  coreutils \
+  zoxide \
+  fzf \
+  gnu-sed \
+  fd \
+  starship \
+  exa \
+  pgcli  \
+  stow \
+  zellij \
+  zoxide \
+  postgresql@14 \
+  flyctl  \
+  coreutils \
+  gnu-sed \
+  jq \
+  terraform \
+  ripgrep \
+  imagemagick \
+  elixir \
+  elixir-ls \
+  helix \
+  goenv \
+
+brew install --cask insomnia \
+  google-chrome \ 
+  slack \ 
+  shiftit \
+  karabiner-elements \
+  font-go-mono-nerd-font \
+  font-hasklig-nerd-font
+
 
 fish_add_path "$HOMEBREW_PREFIX/bin"
 fish_add_path ~/.cargo/bin
@@ -37,20 +63,20 @@ fish_add_path ~/.cargo/bin
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-git clone https://github.com/helix-editor/helix
-cd helix
-cargo install --path helix-term
+# git clone https://github.com/helix-editor/helix
+# cd helix
+# cargo install --path helix-term
 
 
 git config --global url."git@github.com:".insteadOf https://github.com/
 
 
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install IlanCosman/tide@v5
+# fisher install IlanCosman/tide@v5
 fisher install PatrickF1/fzf.fish
 fisher install edc/bass
 
-brew tap homebrew/cask-fonts
-brew install font-hasklig --cask
-
+goenv install 1.19
 go install github.com/go-delve/delve/cmd/dlv@latest
+
+stow config
