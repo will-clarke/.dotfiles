1a107e62fb0a128708c28a34f6f27646df23c508
Will Clarke <will.clarke@justeattakeaway.com>
2023-04-14 09:29:51 +0100

    updates including nvim with lazygit

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -39,3 +39,11 @@ test -f ~/.config/fish/config-local.fish && source ~/.config/fish/config-local.f
 
 
 type -q starship && starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/anaconda3/bin/conda
+    eval /opt/homebrew/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
