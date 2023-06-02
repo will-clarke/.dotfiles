f41f00e9b0bb63cfbeb2f10051166e0495843f8a
Will Clarke <will.clarke@justeattakeaway.com>
2023-06-02 17:39:46 +0100

    zk is cool

--- config/.config/nvim/after/plugin/cmp.lua
+++ config/.config/nvim/after/plugin/cmp.lua
@@ -22,15 +22,18 @@ cmp.setup {
         { name = 'orgmode' }
     }),
     mapping = cmp.mapping.preset.insert({
+        -- do we need more 'complete' mappings?
         ['<C-m>'] = cmp.mapping.complete(),
         ['<C-j>'] = cmp.mapping.select_next_item(),
         ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Tab>'] = cmp.mapping.complete(),
+        ['<C-Tab>'] = cmp.mapping.confirm(),
+        ['<C-CR>'] = cmp.mapping.confirm(),
+        ['<C-d>'] = cmp.mapping.confirm(),
         ['<C-e>'] = cmp.mapping.abort(),
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.expand_or_locally_jumpable() then
+            if luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
             else
                 fallback()
@@ -43,28 +46,6 @@ cmp.setup {
                 fallback()
             end
         end, { "i", "s" }),
-        -- ["<Tab>"] = cmp.mapping(function(fallback)
-        --     if cmp.visible() then
-        --         cmp.select_next_item()
-        --         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-        --         -- they way you will only jump inside the snippet region
-        --     elseif luasnip.expand_or_locally_jumpable() then
-        --         luasnip.expand_or_jump()
-        --     elseif has_words_before() then
-        --         cmp.complete()
-        --     else
-        --         fallback()
-        --     end
-        -- end, { "i", "s" }),
-        -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-        --     if cmp.visible() then
-        --         cmp.select_prev_item()
-        --     elseif luasnip.jumpable(-1) then
-        --         luasnip.jump(-1)
-        --     else
-        --         fallback()
-        --     end
-        -- end, { "i", "s" }),
         ["<CR>"] = cmp.mapping({
             i = function(fallback)
                 if cmp.visible() and cmp.get_active_entry() then
