9158e95a90832153d734f1552d8e038e61aaeec7
William Clarke <wmmclarke@gmail.com>
2023-06-06 19:48:02 +0100

    another dap test

--- config/.config/nvim/after/plugin/dap.lua
+++ config/.config/nvim/after/plugin/dap.lua
@@ -1,4 +1,43 @@
 local dap = require('dap')
+
+
+
+-- https://github.com/mfussenegger/nvim-dap/wiki/C-C---Rust-(gdb-via--vscode-cpptools)
+dap.adapters.cppdbg = {
+    id = 'cppdbg',
+    type = 'executable',
+    command = os.getenv("HOME") .. '/code/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+}
+
+
+dap.configurations.cpp = {
+    {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtEntry = true,
+    },
+    {
+        name = 'Attach to gdbserver :1234',
+        type = 'cppdbg',
+        request = 'launch',
+        MIMode = 'gdb',
+        miDebuggerServerAddress = 'localhost:1234',
+        miDebuggerPath = '/usr/bin/gdb',
+        cwd = '${workspaceFolder}',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+    },
+}
+
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
 -- dap.adapters.lldb = {
 --     type = 'executable',
 --     command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
