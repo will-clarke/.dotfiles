d68e9728b4514e0ebcbaf35b6761e5179e8e625f
Will Clarke <will.clarke@justeattakeaway.com>
2023-06-07 16:15:15 +0100

    de-tpm-ify tmux a little by removing plugins and copying settings

--- config/.config/nvim/after/plugin/mappings.lua
+++ config/.config/nvim/after/plugin/mappings.lua
@@ -286,3 +286,28 @@ vim.keymap.set({ 'n', 't' }, '<A-l>', '<CMD>NavigatorRight<CR>')
 vim.keymap.set({ 'n', 't' }, '<A-k>', '<CMD>NavigatorUp<CR>')
 vim.keymap.set({ 'n', 't' }, '<A-j>', '<CMD>NavigatorDown<CR>')
 vim.keymap.set({ 'n', 't' }, '<A-p>', '<CMD>NavigatorPrevious<CR>')
+--
+vim.keymap.set({ 'n', 't' }, '<A-H>', '<C-W><')
+vim.keymap.set({ 'n', 't' }, '<A-L>', '')
+vim.keymap.set({ 'n', 't' }, '<A-K>', ':exe ":norm <C-W>-<CR>"')
+vim.keymap.set({ 'n', 't' }, '<A-J>', ':norm "<C-W>+"<CR>')
+--
+--
+-- " decrease width
+-- nnoremap <C-H> <C-W><
+-- " increase width
+-- nnoremap <C-L> <C-W>>
+-- " decrease height
+-- nnoremap <C-K> <C-W>-
+-- " increase height
+-- nnoremap <C-J> <C-W>+
+-- " move window in normal
+-- nnoremap <A-h> <C-w>h
+-- nnoremap <A-j> <C-w>j
+-- nnoremap <A-k> <C-w>k
+-- nnoremap <A-l> <C-w>l
+-- " create windows using H, J, K, L
+-- nnoremap <A-H> <C-w>v
+-- nnoremap <A-J> <C-w>s<C-w>k
+-- nnoremap <A-K> <C-w>s
+-- nnoremap <A-L> <C-w>v<C-w>h
