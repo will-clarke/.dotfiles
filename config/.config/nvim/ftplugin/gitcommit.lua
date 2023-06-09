e6c17f66c6b036db470116800dd433d084410c56
William Clarke <wmmclarke@gmail.com>
2023-06-09 23:21:58 +0100

    Update a few bits and bobs

--- config/.config/nvim/ftplugin/gitcommit.lua
+++ config/.config/nvim/ftplugin/gitcommit.lua
@@ -1,6 +1,6 @@
 local function map(...) vim.api.nvim_buf_set_keymap(0, ...) end
 local opts = { noremap = true, silent = false }
 
--- Open the link under the caret.
+-- commit a la magit
 map("n", "<C-C><C-C>", "ZZ", opts)
 map("i", "<C-C><C-C>", "<C-o>ZZ", opts)
