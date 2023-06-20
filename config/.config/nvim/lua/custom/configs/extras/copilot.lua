726d594cc3515480696022906b688f284667a63b
William Clarke <wmmclarke@gmail.com>
2023-06-20 21:43:24 +0100

    New Copilot Mappings

--- config/.config/nvim/lua/custom/configs/extras/copilot.lua
+++ config/.config/nvim/lua/custom/configs/extras/copilot.lua
@@ -12,61 +12,58 @@
 --- }
 --- ```
 
-
-
 local options = {
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    debounce = 75,
-    keymap = {
-      accept = "<M-j>",
-      accept_word = false,
-      accept_line = "<C-j>",
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 16.x
-  server_opts_overrides = {},
+	panel = {
+		enabled = true,
+		auto_refresh = false,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<CR>",
+			refresh = "gr",
+			open = "<M-CR>",
+		},
+		layout = {
+			position = "bottom", -- | top | left | right
+			ratio = 0.4,
+		},
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		debounce = 75,
+		keymap = {
+			accept = "<M-m>",
+			accept_word = false,
+			accept_line = "<C-m>",
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+	copilot_node_command = "node", -- Node.js version must be > 16.x
+	server_opts_overrides = {},
 }
-
 
 ---@type NvPluginSpec
 local spec = {
-  {
-    "zbirenbaum/copilot.lua",
-    event = { "InsertEnter" },
-    cmd = { "Copilot" },
-    opts = options,
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		event = { "InsertEnter" },
+		cmd = { "Copilot" },
+		opts = options,
+	},
 }
 
 return spec
