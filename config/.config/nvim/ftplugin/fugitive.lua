1df510c5eb494beb2e242dcd78a8611aea3eebec
Will Clarke <will.clarke@justeattakeaway.com>
2023-10-26 10:02:26 +0100

    Maybe git should be sync actually

--- config/.config/nvim/ftplugin/fugitive.lua
+++ config/.config/nvim/ftplugin/fugitive.lua
@@ -5,5 +5,5 @@ local opts = { noremap = true, silent = false }
 
 map("n", "<tab>", ":normal =<CR>", opts)
 map("n", "cn", ":<C-U>Git commit --no-verify<CR>", opts)
-map("n", "p", ":<C-U>Git! pull<CR>", opts)
-map("n", "P", ":<C-U>Git! push<CR>", opts)
+map("n", "p", ":<C-U>Git pull<CR>", opts)
+map("n", "P", ":<C-U>Git push<CR>", opts)
