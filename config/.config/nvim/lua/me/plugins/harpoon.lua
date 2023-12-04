54682fd85bb201ae7dc2d38672225f23c46db79c
William Clarke <wmmclarke@gmail.com>
2023-12-04 09:34:52 +0000

    Readline & harpoon updating / deleting

--- config/.config/nvim/lua/me/plugins/harpoon.lua
+++ config/.config/nvim/lua/me/plugins/harpoon.lua
@@ -1,6 +1,12 @@
 return {
-	"ThePrimeagen/harpoon",
-	config = function()
-		require("harpoon").setup({})
-	end,
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { { "nvim-lua/plenary.nvim" } },
+    config = function()
+        local harpoon = require("harpoon")
+        harpoon:setup()
+    end
+    -- config = function()
+    -- require("harpoon").setup({})
+    -- end,
 }
