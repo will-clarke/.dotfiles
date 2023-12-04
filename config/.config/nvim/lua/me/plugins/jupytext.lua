2f5afddcc7a4330e6b4bd7fb7b7e2203daae402d
William Clarke <wmmclarke@gmail.com>
2023-12-04 16:33:10 +0000

    Molten > Magma for ipynb

---
+++ config/.config/nvim/lua/me/plugins/jupytext.lua
@@ -0,0 +1,9 @@
+return {
+	"GCBallesteros/jupytext.nvim",
+	config = true,
+	opts = {
+		style = "markdown",
+	},
+	ft = { "*.py", "*ipynb" },
+	lazy = false,
+}
--- config/.config/nvim/lua/me/plugins/molten.lua
+++ config/.config/nvim/lua/me/plugins/molten.lua
@@ -2,6 +2,7 @@ return {
 	"benlubas/molten-nvim",
 	-- version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
 	dependencies = { "3rd/image.nvim" },
+	lazy = false,
 	build = ":UpdateRemotePlugins",
 	init = function()
 		-- these are examples, not defaults. Please see the readme
