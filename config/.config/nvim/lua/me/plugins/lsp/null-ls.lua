e1b9e08068cf79a6a8e5c94fd16fc720123f35b6
William Clarke <wmmclarke@gmail.com>
2024-02-01 14:55:09 +0000

    Mypy options

--- config/.config/nvim/lua/me/plugins/lsp/null-ls.lua
+++ config/.config/nvim/lua/me/plugins/lsp/null-ls.lua
@@ -32,7 +32,7 @@ return {
 				null_ls.builtins.diagnostics.mypy.with({
 					extra_args = function()
 						local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
-						return { "--python-executable", virtual .. "/bin/python3" }
+						return { "--ignore-missing-imports", "--python-executable", virtual .. "/bin/python3" }
 					end,
 				}),
 				null_ls.builtins.formatting.stylua,
